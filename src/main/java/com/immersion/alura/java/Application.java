df3b675cb564a2b432f4222de6f876df3cf5487e
Lauro Correia Silveira <lauro.silveira@outlook.com.br>
2022-07-23 22:38:49 +0200

    - User Api key has been removed from properties - Refactored Enum to return endpoint complete - Renamed Enum

--- src/main/java/com/immersion/alura/java/Application.java
+++ src/main/java/com/immersion/alura/java/Application.java
@@ -1,6 +1,5 @@
 package com.immersion.alura.java;
 
-import com.immersion.alura.java.conf.PropertiesConfig;
 import com.immersion.alura.java.domain.ProcessData;
 import com.immersion.alura.java.domain.StickerGeneratorDelegator;
 import com.immersion.alura.java.domain.StickerGeneratorIMDB;
@@ -9,19 +8,18 @@ import com.immersion.alura.java.mapper.JacksonParser;
 import com.immersion.alura.java.model.Movie;
 import com.immersion.alura.java.model.Movies;
 import com.immersion.alura.java.model.NasaContent;
-import com.immersion.alura.java.model.enums.GetUrlEnum;
 import com.immersion.alura.java.service.HttpRequestService;
 
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static com.immersion.alura.java.model.enums.GetUrlEnum.URL_MARVEL;
+import static com.immersion.alura.java.enums.EndpointsEnum.URL_MOST_POPULAR_MOVIES;
+import static com.immersion.alura.java.enums.EndpointsEnum.URL_TOP_250_MOVIES;
 
 public class Application {
 
     private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
-    private static final PropertiesConfig propertiesConfig = new PropertiesConfig();
     private static final HttpRequestService httpRequestService = new HttpRequestService();
     private static final ProcessData processData = new ProcessData();
     private static final JacksonParser jacksonParser = new JacksonParser();
@@ -29,25 +27,16 @@ public class Application {
     //Patter Java Delegate
     private static final StickerGeneratorDelegator stickerGeneratorIMDB = new StickerGeneratorDelegator(new StickerGeneratorIMDB());
     private static final StickerGeneratorDelegator stickerGeneratorNasa = new StickerGeneratorDelegator(new StickerGeneratorNasa());
+
     public static void main(String[] args) {
-        final var top250Url = propertiesConfig.getProperties().getProperty(GetUrlEnum.URL_TOP_250_MOVIES.getUrl())
-                + propertiesConfig.getProperties().get(GetUrlEnum.URL_TOP_250_MOVIES.getApiKey());
-
-
-        final var mostPopularMoviesUrl = propertiesConfig.getProperties()
-                .getProperty(GetUrlEnum.URL_MOST_POPULAR_MOVIES.getUrl())
-                + propertiesConfig.getProperties().getProperty(GetUrlEnum.URL_MOST_POPULAR_MOVIES.getApiKey());
-
-        final var nasaUrl = propertiesConfig.getProperties().getProperty(GetUrlEnum.URL_ASTRONOMIC_DAY_NASA.getUrl())
-                + propertiesConfig.getProperties().getProperty(GetUrlEnum.URL_ASTRONOMIC_DAY_NASA.getApiKey());
 
         LOGGER.log(Level.INFO, "Request to IMD API to get Top 250 Movies");
-        final var responseTopMovies = httpRequestService.getRequestIMDB(top250Url);
+        final var responseTopMovies = httpRequestService.getRequestIMDB(URL_TOP_250_MOVIES.getEndpoint());
         final var movies = jacksonParser.parseToJavaObject(responseTopMovies, Movies.class);
         processData.printRequest(movies);
 
         LOGGER.log(Level.INFO, "Request to IMD API to get Top Most Popular Movies");
-        final var responseMoviesPopular = httpRequestService.getRequestIMDB(mostPopularMoviesUrl);
+        final var responseMoviesPopular = httpRequestService.getRequestIMDB(URL_MOST_POPULAR_MOVIES.getEndpoint());
         final var moviesMostPopular = jacksonParser.parseToJavaObject(responseMoviesPopular, Movies.class);
         processData.printRequest(moviesMostPopular);
 
@@ -55,18 +44,18 @@ public class Application {
         final Optional<Movie> movie = moviesMostPopular.getItems().stream().findAny();
         stickerGeneratorIMDB.stickerGenerator(movie.get().getBanner(), movie.get().getTitle(), movie.get().getImDbRating());
 
+        LOGGER.log(Level.INFO, "Request to Nasa Api to get Astronomic Day Photo...");
+        final String nasaResponse = httpRequestService.getNasaRequest(null);
+        NasaContent nasaContent = jacksonParser.parseToJavaObject(nasaResponse, NasaContent.class);
+        stickerGeneratorNasa.stickerGenerator(nasaContent.urlImage(), nasaContent.title(), null);
+
         //TODO: Implement Marven endpoint
 /*
         LOGGER.log(Level.INFO, "Request to Marvel Api to get list of all characters");
         String responseMarvelJson = httpRequestService.getMarvelRequest(propertiesConfig.getProperties().getProperty(URL_MARVEL.getUrl())
-                .concat(propertiesConfig.getProperties().getProperty(GetUrlEnum.K)));
+                .concat(propertiesConfig.getProperties().getProperty(EndpointsEnum.K)));
         System.out.println("Marvel response JSon " + responseMarvelJson);
 */
-
-        LOGGER.log(Level.INFO, "Request to Nasa Api to get Astronomic Day Photo...");
-        final String nasaResponse = httpRequestService.getNasaRequest(nasaUrl);
-        NasaContent nasaContent = jacksonParser.parseToJavaObject(nasaResponse, NasaContent.class);
-        stickerGeneratorNasa.stickerGenerator(nasaContent.urlImage(), nasaContent.title(), null);
 
     }
 }
---
+++ src/main/java/com/immersion/alura/java/enums/EndpointsEnum.java
@@ -0,0 +1,32 @@
+package com.immersion.alura.java.enums;
+
+import com.immersion.alura.java.conf.PropertiesConfig;
+
+public enum EndpointsEnum {
+    URL_MOST_POPULAR_MOVIES("api.imdb.url-most-popular-movies", "API_KEY_IMDB"),
+    URL_TOP_250_MOVIES("api.imdb.url-top-250-movies", "API_KEY_IMDB"),
+    URL_ASTRONOMIC_DAY_NASA("api.nasa.url.photo-astronomic-of-the-day", "API_KEY_NASA"),
+    URL_MARVEL("api.marvel.url-list-of-all.characters", "API_KEY_MARVEL");
+
+    private final String url;
+    private final String apiKey;
+    private final String endpoint;
+
+    EndpointsEnum(final String url, final String apiKey) {
+        this.url = new PropertiesConfig().getProperties().getProperty(url);
+        this.apiKey = System.getenv(apiKey);
+        this.endpoint = getUrl() + getApiKey();
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+}
